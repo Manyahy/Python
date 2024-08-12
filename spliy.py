e71a5199b737ae3b67a4971720ef8f5976c12a77
Manya H Y <hymanya5@gmail.com>
2024-08-12 15:30:20 +0530

    Create spliy.py

---
+++ spliy.py
@@ -0,0 +1,8 @@
+str1 = "Welcome-New-Horizon-College-Of-Engineering"
+print(str1)
+words = str1.split('-')
+print(words)
+new_str =sorted(words)
+print(new_str)
+hyphen = '-'.join(new_str)
+print(hyphen)
